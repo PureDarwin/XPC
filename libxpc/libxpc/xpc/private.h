094a33bbe255df00c4db66b228b791d89b367a7e
Cliff Sekel <cds87@live.com>
2016-06-08 08:37:04 -0400

    Libxpc added for PureDarwin 14.5(OS X 10.10.5)
    files added

---
+++ libxpc/libxpc/xpc/private.h
@@ -0,0 +1,16 @@
+#ifndef XPC_PRIVATE_H_
+#define XPC_PRIVATE_H_
+#include <xpc/xpc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int _xpc_runtime_is_app_sandboxed();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ libxpc/libxpc/xpc_private.c
@@ -0,0 +1,7 @@
+#include <xpc/private.h>
+
+int _xpc_runtime_is_app_sandboxed()
+{
+	return 0;
+}
+
