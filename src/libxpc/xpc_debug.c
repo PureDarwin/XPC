552f8b6690da893fa2585d67b5443d51eee2f441
William Kent <wjk@sunsol.me>
2018-08-30 18:41:49 -0400

    Make xpc_api_misuse() a printf-like function

--- src/libxpc/xpc_debug.c
+++ src/libxpc/xpc_debug.c
@@ -6,8 +6,13 @@ asm(".desc __crashreporter_info__, 0x10");
 static char *xpc_api_misuse_reason = NULL;
 
 __attribute__((visibility("hidden"), noreturn))
-void xpc_api_misuse(const char *info) {
-	xpc_api_misuse_reason = __crashreporter_info__ = info;
+void xpc_api_misuse(const char *info, ...) {
+	va_list ap;
+	va_start(ap, info);
+	vasprintf(&xpc_api_misuse_reason, info, ap);
+	va_end(ap);
+
+	__crashreporter_info__ = xpc_api_misuse_reason;
 	abort();
 }
 
