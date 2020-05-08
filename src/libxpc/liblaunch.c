69eb71b94add5e8517078c5ef7139f16367e1514
William Kent <wjk011@gmail.com>
2020-05-07 20:34:43 -0400

    Make launch_data_t equivalent to xpc_object_t
    This lets me get rid of ld2xpc(), which no longer
    exists in Apple's libxpc as of 10.15, which in turn
    fixes the launchd build.

--- src/libxpc/liblaunch.c
+++ src/libxpc/liblaunch.c
@@ -47,6 +47,10 @@
 #include <uuid/uuid.h>
 #include <sys/syscall.h>
 #include <dlfcn.h>
+#include <xpc/xpc.h>
+#include "nv.h"
+
+extern void xpc_api_misuse(const char *reason, ...) __dead2;
 
 #ifdef __LP64__
 /* workaround: 5723161 */
@@ -63,31 +67,6 @@
 #define	CMSG_LEN(l)	\
 	(__DARWIN_ALIGN32(sizeof(struct cmsghdr)) + (l))
 #endif
-
-struct _launch_data {
-	uint64_t type;
-	union {
-		struct {
-			union {
-				launch_data_t *_array;
-				char *string;
-				void *opaque;
-				int64_t __junk;
-			};
-			union {
-				uint64_t _array_cnt;
-				uint64_t string_len;
-				uint64_t opaque_size;
-			};
-		};
-		int64_t fd;
-		uint64_t  mp;
-		uint64_t err;
-		int64_t number;
-		uint64_t boolean; /* We'd use 'bool' but this struct needs to be used under Rosetta, and sizeof(bool) is different between PowerPC and Intel */
-		double float_num;
-	};
-};
 
 #include "bootstrap.h"
 #include "vproc.h"
@@ -317,310 +296,238 @@ out_bad:
 	}
 }
 
+extern xpc_object_t xpc_bool_create_distinct(bool value);
+
 launch_data_t
 launch_data_alloc(launch_data_type_t t)
 {
-	launch_data_t d = calloc(1, sizeof(struct _launch_data));
-
-	if (d) {
-		d->type = t;
-		switch (t) {
-		case LAUNCH_DATA_DICTIONARY:
-		case LAUNCH_DATA_ARRAY:
-			d->_array = malloc(0);
-			break;
-		case LAUNCH_DATA_OPAQUE:
-			d->opaque = malloc(0);
-		default:
-			break;
-		}
-	}
-
-	return d;
+	if (t == LAUNCH_DATA_DICTIONARY) return (launch_data_t)xpc_dictionary_create(NULL, NULL, 0);
+	else if (t == LAUNCH_DATA_ARRAY) return (launch_data_t)xpc_array_create(NULL, 0);
+	else if (t == LAUNCH_DATA_OPAQUE) return xpc_data_create(NULL, 0);
+	else if (t == LAUNCH_DATA_STRING) return xpc_string_create("");
+	else if (t == LAUNCH_DATA_BOOL) return xpc_bool_create_distinct(false);
+	else if (t == LAUNCH_DATA_INTEGER) return xpc_int64_create(0);
+	else if (t == LAUNCH_DATA_REAL) return xpc_double_create(0);
+	else if (t == LAUNCH_DATA_ERRNO) return xpc_uint64_create(0);
+	else xpc_api_misuse("You cannot create a launch_data_t of type %d anymore", t);
 }
 
 launch_data_type_t
 launch_data_get_type(launch_data_t d)
 {
-	return d->type;
+	xpc_type_t type = xpc_get_type(d);
+	if (type == XPC_TYPE_DICTIONARY) return LAUNCH_DATA_DICTIONARY;
+	else if (type == XPC_TYPE_ARRAY) return LAUNCH_DATA_ARRAY;
+	else if (type == XPC_TYPE_FD) return LAUNCH_DATA_FD;
+	else if (type == XPC_TYPE_INT64) return LAUNCH_DATA_INTEGER;
+	else if (type == XPC_TYPE_DOUBLE) return LAUNCH_DATA_REAL;
+	else if (type == XPC_TYPE_BOOL) return LAUNCH_DATA_BOOL;
+	else if (type == XPC_TYPE_DATA) return LAUNCH_DATA_OPAQUE;
+	else if (type == XPC_TYPE_ERROR) return LAUNCH_DATA_ERRNO;
+	else if (type == XPC_TYPE_ENDPOINT) return LAUNCH_DATA_MACHPORT;
+
+	xpc_api_misuse("Unknown xpc_type_t %p, this should not happen", type);
 }
 
 void
 launch_data_free(launch_data_t d)
 {
-	size_t i;
-
-	switch (d->type) {
-	case LAUNCH_DATA_DICTIONARY:
-	case LAUNCH_DATA_ARRAY:
-		for (i = 0; i < d->_array_cnt; i++) {
-			if (d->_array[i]) {
-				launch_data_free(d->_array[i]);
-			}
-		}
-		free(d->_array);
-		break;
-	case LAUNCH_DATA_STRING:
-		if (d->string)
-			free(d->string);
-		break;
-	case LAUNCH_DATA_OPAQUE:
-		if (d->opaque)
-			free(d->opaque);
-		break;
-	default:
-		break;
-	}
-	free(d);
+	xpc_release(d);
 }
 
 size_t
 launch_data_dict_get_count(launch_data_t dict)
 {
-	return dict->_array_cnt / 2;
+	return xpc_dictionary_get_count(dict);
 }
 
 bool
 launch_data_dict_insert(launch_data_t dict, launch_data_t what, const char *key)
 {
-	size_t i;
-	launch_data_t thekey = launch_data_alloc(LAUNCH_DATA_STRING);
-
-	launch_data_set_string(thekey, key);
-
-	for (i = 0; i < dict->_array_cnt; i += 2) {
-		if (!strcasecmp(key, dict->_array[i]->string)) {
-			launch_data_array_set_index(dict, thekey, i);
-			launch_data_array_set_index(dict, what, i + 1);
-			return true;
-		}
-	}
-	launch_data_array_set_index(dict, thekey, i);
-	launch_data_array_set_index(dict, what, i + 1);
+	xpc_dictionary_set_value(dict, key, what);
 	return true;
 }
 
 launch_data_t
 launch_data_dict_lookup(launch_data_t dict, const char *key)
 {
-	size_t i;
-
-	if (LAUNCH_DATA_DICTIONARY != dict->type)
-		return NULL;
-
-	for (i = 0; i < dict->_array_cnt; i += 2) {
-		if (!strcasecmp(key, dict->_array[i]->string))
-			return dict->_array[i + 1];
-	}
-
-	return NULL;
+	return xpc_dictionary_get_value(dict, key);
 }
 
 bool
 launch_data_dict_remove(launch_data_t dict, const char *key)
 {
-	size_t i;
-
-	for (i = 0; i < dict->_array_cnt; i += 2) {
-		if (!strcasecmp(key, dict->_array[i]->string))
-			break;
-	}
-	if (i == dict->_array_cnt)
-		return false;
-	launch_data_free(dict->_array[i]);
-	launch_data_free(dict->_array[i + 1]);
-	memmove(dict->_array + i, dict->_array + i + 2, (dict->_array_cnt - (i + 2)) * sizeof(launch_data_t));
-	dict->_array_cnt -= 2;
+	xpc_dictionary_set_value(dict, key, NULL);
 	return true;
 }
 
 void
 launch_data_dict_iterate(launch_data_t dict, void (*cb)(launch_data_t, const char *, void *), void *context)
 {
-	size_t i;
-
-	if (LAUNCH_DATA_DICTIONARY != dict->type) {
-		return;
-	}
-
-	for (i = 0; i < dict->_array_cnt; i += 2) {
-		cb(dict->_array[i + 1], dict->_array[i]->string, context);
-	}
+	xpc_dictionary_apply(dict, ^bool(const char *key, xpc_object_t value) {
+		cb(value, key, context);
+		return true;
+	});
 }
 
 bool
 launch_data_array_set_index(launch_data_t where, launch_data_t what, size_t ind)
 {
-	if ((ind + 1) >= where->_array_cnt) {
-		where->_array = reallocf(where->_array, (ind + 1) * sizeof(launch_data_t));
-		memset(where->_array + where->_array_cnt, 0, (ind + 1 - where->_array_cnt) * sizeof(launch_data_t));
-		where->_array_cnt = ind + 1;
-	}
-
-	if (where->_array[ind]) {
-		launch_data_free(where->_array[ind]);
-	}
-
-	where->_array[ind] = what;
+	xpc_array_set_value(where, ind, what);
 	return true;
 }
 
 launch_data_t
 launch_data_array_get_index(launch_data_t where, size_t ind)
 {
-	if (LAUNCH_DATA_ARRAY != where->type || ind >= where->_array_cnt) {
-		return NULL;
-	} else {
-		return where->_array[ind];
-	}
+	return xpc_array_get_value(where, ind);
 }
 
 launch_data_t
 launch_data_array_pop_first(launch_data_t where)
 {
-	launch_data_t r = NULL;
+	xpc_object_t copy = xpc_array_create(NULL, 0);
 
-	if (where->_array_cnt > 0) {
-		r = where->_array[0];
-		memmove(where->_array, where->_array + 1, (where->_array_cnt - 1) * sizeof(launch_data_t));
-		where->_array_cnt--;
-	}
-	return r;
+	__block bool first = true;
+	xpc_array_apply(where, ^bool(size_t index, xpc_object_t value) {
+		if (first) {
+			first = false;
+		} else {
+			xpc_array_append_value(copy, value);
+		}
+
+		return true;
+	});
+
+	xpc_release(where);
+	xpc_retain(copy);
+
+	return copy;
 }
 
 size_t
 launch_data_array_get_count(launch_data_t where)
 {
-	if (LAUNCH_DATA_ARRAY != where->type)
-		return 0;
-	return where->_array_cnt;
+	return xpc_array_get_count(where);
 }
 
 bool
 launch_data_set_errno(launch_data_t d, int e)
 {
-	d->err = e;
 	return true;
 }
 
 bool
 launch_data_set_fd(launch_data_t d, int fd)
 {
-	d->fd = fd;
-	return true;
+	xpc_api_misuse("This API is no longer implemented.");
 }
 
 bool
 launch_data_set_machport(launch_data_t d, mach_port_t p)
 {
-	d->mp = p;
-	return true;
+	xpc_api_misuse("This API is no longer implemented.");
 }
+
+extern void xpc_int64_set_value(xpc_object_t xo, uint64_t value);
 
 bool
 launch_data_set_integer(launch_data_t d, long long n)
 {
-	d->number = n;
+	xpc_int64_set_value(d, n);
 	return true;
 }
+
+extern void xpc_bool_set_value(xpc_object_t xo, bool value);
 
 bool
 launch_data_set_bool(launch_data_t d, bool b)
 {
-	d->boolean = b;
+	xpc_bool_set_value(d, b);
 	return true;
 }
+
+extern void xpc_double_set_value(xpc_object_t xo, double value);
 
 bool
 launch_data_set_real(launch_data_t d, double n)
 {
-	d->float_num = n;
+	xpc_double_set_value(d, n);
 	return true;
 }
+
+extern void xpc_string_set_value(xpc_object_t xo, const char *value);
 
 bool
 launch_data_set_string(launch_data_t d, const char *s)
 {
-	if (d->string)
-		free(d->string);
-	d->string = strdup(s);
-	if (d->string) {
-		d->string_len = strlen(d->string);
-		return true;
-	}
+	xpc_string_set_value(d, s);
 	return false;
 }
+
+extern void xpc_data_set_bytes(xpc_object_t xo, void *buffer, size_t size);
 
 bool
 launch_data_set_opaque(launch_data_t d, const void *o, size_t os)
 {
-	d->opaque_size = os;
-	if (d->opaque)
-		free(d->opaque);
-	d->opaque = malloc(os);
-	if (d->opaque) {
-		memcpy(d->opaque, o, os);
-		return true;
-	}
-	return false;
+	xpc_data_set_bytes(d, o, os);
+	return true;
 }
 
 int
 launch_data_get_errno(launch_data_t d)
 {
-	return d->err;
+	return xpc_uint64_get_value(d);
 }
 
 int
 launch_data_get_fd(launch_data_t d)
 {
-	return d->fd;
+	return xpc_fd_dup(d);
 }
+
+extern mach_port_t xpc_object_get_machport(xpc_object_t obj);
 
 mach_port_t
 launch_data_get_machport(launch_data_t d)
 {
-	return d->mp;
+	return xpc_object_get_machport(d);
 }
 
 long long
 launch_data_get_integer(launch_data_t d)
 {
-	return d->number;
+	return xpc_int64_get_value(d);
 }
 
 bool
 launch_data_get_bool(launch_data_t d)
 {
-	return d->boolean;
+	return xpc_bool_get_value(d);
 }
 
 double
 launch_data_get_real(launch_data_t d)
 {
-	return d->float_num;
+	return xpc_double_get_value(d);
 }
 
 const char *
 launch_data_get_string(launch_data_t d)
 {
-	if (LAUNCH_DATA_STRING != d->type)
-		return NULL;
-	return d->string;
+	return xpc_string_get_string_ptr(d);
 }
 
 void *
 launch_data_get_opaque(launch_data_t d)
 {
-	if (LAUNCH_DATA_OPAQUE != d->type)
-		return NULL;
-	return d->opaque;
+	return xpc_data_get_bytes_ptr(d);
 }
 
 size_t
 launch_data_get_opaque_size(launch_data_t d)
 {
-	return d->opaque_size;
+	return xpc_data_get_length(d);
 }
 
 int
@@ -698,180 +605,37 @@ launchd_close(launch_t lh, typeof(close) closefunc)
 
 #define ROUND_TO_64BIT_WORD_SIZE(x)	((x + 7) & ~7)
 
+extern nvlist_t *xpc2nv(xpc_object_t xo, int64_t (^port_serializer)(mach_port_t port));
+extern xpc_object_t nv2xpc(const nvlist_t *nv, mach_port_t (^port_deserializer)(int64_t port_id));
+
 size_t
 launch_data_pack(launch_data_t d, void *where, size_t len, int *fd_where, size_t *fd_cnt)
 {
-	launch_data_t o_in_w = where;
-	size_t i, rsz, node_data_len = sizeof(struct _launch_data);
+	nvlist_t *nvl = xpc2nv(d, ^int64_t(mach_port_t port) {
+		xpc_api_misuse("Cannot currently serialize mach ports in launch_data_pack()");
+	});
 
-	if (node_data_len > len) {
-		return 0;
+	size_t size;
+	void *data = nvlist_pack(nvl, &size);
+	if (size >= len) {
+		memcpy(where, data, size);
 	}
 
-	where += node_data_len;
+	free(data);
+	nvlist_destroy(nvl);
 
-	o_in_w->type = host2wire(d->type);
-
-	size_t pad_len = 0;
-	switch (d->type) {
-	case LAUNCH_DATA_INTEGER:
-		o_in_w->number = host2wire(d->number);
-		break;
-	case LAUNCH_DATA_REAL:
-		o_in_w->float_num = host2wire_f(d->float_num);
-		break;
-	case LAUNCH_DATA_BOOL:
-		o_in_w->boolean = host2wire(d->boolean);
-		break;
-	case LAUNCH_DATA_ERRNO:
-		o_in_w->err = host2wire(d->err);
-		break;
-	case LAUNCH_DATA_FD:
-		o_in_w->fd = host2wire(d->fd);
-		if (fd_where && d->fd != -1) {
-			fd_where[*fd_cnt] = d->fd;
-			(*fd_cnt)++;
-		}
-		break;
-	case LAUNCH_DATA_STRING:
-		o_in_w->string_len = host2wire(d->string_len);
-		node_data_len += ROUND_TO_64BIT_WORD_SIZE(d->string_len + 1);
-
-		if (node_data_len > len) {
-			return 0;
-		}
-		memcpy(where, d->string, d->string_len + 1);
-
-		/* Zero padded data. */
-		pad_len = ROUND_TO_64BIT_WORD_SIZE(d->string_len + 1) - (d->string_len + 1);
-		bzero(where + d->string_len + 1, pad_len);
-
-		break;
-	case LAUNCH_DATA_OPAQUE:
-		o_in_w->opaque_size = host2wire(d->opaque_size);
-		node_data_len += ROUND_TO_64BIT_WORD_SIZE(d->opaque_size);
-		if (node_data_len > len) {
-			return 0;
-		}
-		memcpy(where, d->opaque, d->opaque_size);
-
-		/* Zero padded data. */
-		pad_len = ROUND_TO_64BIT_WORD_SIZE(d->opaque_size) - d->opaque_size;
-		bzero(where + d->opaque_size, pad_len);
-
-		break;
-	case LAUNCH_DATA_DICTIONARY:
-	case LAUNCH_DATA_ARRAY:
-		o_in_w->_array_cnt = host2wire(d->_array_cnt);
-		node_data_len += d->_array_cnt * sizeof(uint64_t);
-		if (node_data_len > len) {
-			return 0;
-		}
-
-		where += d->_array_cnt * sizeof(uint64_t);
-
-		for (i = 0; i < d->_array_cnt; i++) {
-			rsz = launch_data_pack(d->_array[i], where, len - node_data_len, fd_where, fd_cnt);
-			if (rsz == 0) {
-				return 0;
-			}
-			where += rsz;
-			node_data_len += rsz;
-		}
-		break;
-	default:
-		break;
-	}
-
-	return node_data_len;
+	return size;
 }
 
 launch_data_t
 launch_data_unpack(void *data, size_t data_size, int *fds, size_t fd_cnt, size_t *data_offset, size_t *fdoffset)
 {
-	launch_data_t r = data + *data_offset;
-	size_t i, tmpcnt;
-
-	//Check for integer underflow
-	if (data_size < *data_offset)
-		return NULL;
-
-	if ((data_size - *data_offset) < sizeof(struct _launch_data))
-		return NULL;
-	*data_offset += sizeof(struct _launch_data);
-
-	switch (big2wire(r->type)) {
-	case LAUNCH_DATA_DICTIONARY:
-	case LAUNCH_DATA_ARRAY:
-		tmpcnt = big2wire(r->_array_cnt);
-
-		//Check for integer overflows
-		if (tmpcnt > SIZE_MAX / sizeof(uint64_t)) {
-			errno = EAGAIN;
-			return NULL;
-		}
-
-		if ((data_size - *data_offset) < (tmpcnt * sizeof(uint64_t))) {
-			errno = EAGAIN;
-			return NULL;
-		}
-		r->_array = data + *data_offset;
-		*data_offset += tmpcnt * sizeof(uint64_t);
-		for (i = 0; i < tmpcnt; i++) {
-			r->_array[i] = launch_data_unpack(data, data_size, fds, fd_cnt, data_offset, fdoffset);
-			if (r->_array[i] == NULL)
-				return NULL;
-		}
-		r->_array_cnt = tmpcnt;
-		break;
-	case LAUNCH_DATA_STRING:
-		tmpcnt = big2wire(r->string_len);
-		if ((data_size - *data_offset) < (tmpcnt + 1)) {
-			errno = EAGAIN;
-			return NULL;
-		}
-		r->string = data + *data_offset;
-		r->string_len = tmpcnt;
-		*data_offset += ROUND_TO_64BIT_WORD_SIZE(tmpcnt + 1);
-		break;
-	case LAUNCH_DATA_OPAQUE:
-		tmpcnt = big2wire(r->opaque_size);
-		if ((data_size - *data_offset) < tmpcnt) {
-			errno = EAGAIN;
-			return NULL;
-		}
-		r->opaque = data + *data_offset;
-		r->opaque_size = tmpcnt;
-		*data_offset += ROUND_TO_64BIT_WORD_SIZE(tmpcnt);
-		break;
-	case LAUNCH_DATA_FD:
-		if (r->fd != -1 && fd_cnt > *fdoffset) {
-			r->fd = _fd(fds[*fdoffset]);
-			*fdoffset += 1;
-		}
-		break;
-	case LAUNCH_DATA_INTEGER:
-		r->number = big2wire(r->number);
-		break;
-	case LAUNCH_DATA_REAL:
-		r->float_num = big2wire_f(r->float_num);
-		break;
-	case LAUNCH_DATA_BOOL:
-		r->boolean = big2wire(r->boolean);
-		break;
-	case LAUNCH_DATA_ERRNO:
-		r->err = big2wire(r->err);
-	case LAUNCH_DATA_MACHPORT:
-		break;
-	default:
-		errno = EINVAL;
-		return NULL;
-		break;
-	}
-
-	r->type = big2wire(r->type);
-
-	return r;
+	nvlist_t *nvl = nvlist_unpack(data, data_size);
+	xpc_object_t xo = nv2xpc(nvl, ^mach_port_t(int64_t port_id) {
+		xpc_api_misuse("Should not be called");
+	});
+	nvlist_destroy(nvl);
+	return xo;
 }
 
 int
@@ -1329,33 +1093,7 @@ out_bad:
 launch_data_t
 launch_data_copy(launch_data_t o)
 {
-	launch_data_t r = launch_data_alloc(o->type);
-	size_t i;
-
-	free(r->_array);
-	memcpy(r, o, sizeof(struct _launch_data));
-
-	switch (o->type) {
-	case LAUNCH_DATA_DICTIONARY:
-	case LAUNCH_DATA_ARRAY:
-		r->_array = calloc(1, o->_array_cnt * sizeof(launch_data_t));
-		for (i = 0; i < o->_array_cnt; i++) {
-			if (o->_array[i])
-				r->_array[i] = launch_data_copy(o->_array[i]);
-		}
-		break;
-	case LAUNCH_DATA_STRING:
-		r->string = strdup(o->string);
-		break;
-	case LAUNCH_DATA_OPAQUE:
-		r->opaque = malloc(o->opaque_size);
-		memcpy(r->opaque, o->opaque, o->opaque_size);
-		break;
-	default:
-		break;
-	}
-
-	return r;
+	return xpc_retain(o);
 }
 
 int
